9812e98c63158372933e4b2e3b6e2f81aeffde25
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-08-24 14:19:52 +0800

    insert function
    - recursive function that traverses the binary
    tree to find the right spot to insert new value

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ const test = new BinarySortTree([
   1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
 ]);
 
-console.log(test.buildTree());
+test.prettyPrint();
+test.insert(2);
+test.prettyPrint();
--- src/modules/binary-search-tree.js
+++ src/modules/binary-search-tree.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
   }
@@ -9,10 +9,45 @@ class Node {
 export default class BinarySortTree {
   constructor(array) {
     this.array = array;
-    this.root = root;
+    this.root = this.buildTree(this.array);
   }
+
   buildTree(array) {
-    const sortedArray = this.array.sort((a, b) => a - b);
-    return sortedArray;
+    if (array.length == 0) return null;
+    const uniqueArray = [...new Set(array)].sort((a, b) => a - b);
+
+    const mid = Math.floor(uniqueArray.length / 2);
+    const node = new Node(uniqueArray[mid]);
+    node.left = this.buildTree(uniqueArray.slice(0, mid));
+    node.right = this.buildTree(uniqueArray.slice(mid + 1));
+    return node;
+  }
+
+  prettyPrint(node = this.root, prefix = '', isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? '│   ' : '    '}`,
+        false,
+      );
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+
+  insert(value, currentNode = this.root) {
+    if (currentNode == null) return new Node(value);
+    if (currentNode.value === value) return this.root;
+    if (value < currentNode.value) {
+      currentNode.left = this.insert(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.insert(value, currentNode.right);
+    }
+    return currentNode;
   }
 }
