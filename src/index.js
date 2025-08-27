05b5400d3caa26c941e5ffdad3ba9434f598fcd3
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-08-27 14:55:45 +0800

    knightMoves function
    - move the knight according to knight rules until
    reaching end position
    
    - unshift and trace the steps from end to start
    using this.parent and console.log the moves

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './styles.css';
-import { Graph } from './modules/knight-travails';
+import knightMoves from './modules/knight-travails';
 
-const test = new Graph();
-
-console.log(test);
+knightMoves([0, 0], [7, 7]);
--- src/modules/knight-travails.js
+++ src/modules/knight-travails.js
@@ -1,49 +1,59 @@
 class Node {
-  constructor(row, column) {
+  constructor([row, column], parent = null) {
     this.row = row;
     this.column = column;
-    this.moves = this.possibleMoves();
-  }
-
-  possibleMoves() {
-    const moves = [
-      [2, 1],
-      [2, -1],
-      [1, 2],
-      [1, -2],
-      [-2, -1],
-      [-2, 1],
-      [-1, -2],
-      [-1, 2],
-    ];
-    return moves
-      .map(([x, y]) => [x + this.row, y + this.column])
-      .filter(([x, y]) => x >= 0 && x <= 7 && y >= 0 && y <= 7);
+    this.parent = parent;
   }
 }
 
-class Graph {
-  constructor() {
-    this.board = this.populateBoard();
-  }
+const moves = [
+  [2, 1],
+  [2, -1],
+  [1, 2],
+  [1, -2],
+  [-2, -1],
+  [-2, 1],
+  [-1, -2],
+  [-1, 2],
+];
 
-  populateBoard() {
-    const board = [];
-    for (let i = 0; i < 8; i++) {
-      board[i] = [];
-      for (let j = 0; j < 8; j++) {
-        const newNode = new Node(i, j);
-        board[i][j] = newNode;
+function possibleMoves(x, y) {
+  return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
+
+export default function knightMoves(start, end) {
+  const startNode = new Node(start);
+  const queue = [startNode];
+  const visited = new Set();
+  visited.add(`${start[0]}, ${start[1]}`);
+
+  while (queue) {
+    const currentNode = queue.shift();
+
+    if (currentNode.row == end[0] && currentNode.column == end[1]) {
+      const path = [];
+      let temp = currentNode;
+      while (temp !== null) {
+        path.unshift([temp.row, temp.column]);
+        temp = temp.parent;
+      }
+      console.log(
+        `You made it in ${path.length - 1} moves! Here's your path: `,
+      );
+      for (const move in path) console.log(path[move]);
+      return path;
+    }
+
+    for (const [dx, dy] of moves) {
+      const newX = currentNode.row + dx;
+      const newY = currentNode.column + dy;
+      const newMove = `${newX}, ${newY}`;
+
+      if (possibleMoves(newX, newY) && !visited.has(newMove)) {
+        visited.add(newMove);
+        queue.push(new Node([newX, newY], currentNode));
       }
     }
-    return board;
   }
-
-  knightMoves(start, end) {
-    const startNode = this.board[(start[0], start[1])];
-    const endNode = this.board[(end[0], end[1])];
-    if (!startNode || !endNode) throw new Error('Please enter a valid square');
-  }
+  return null;
 }
-
-export { Node, Graph };
